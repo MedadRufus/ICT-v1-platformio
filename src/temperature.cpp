487dcde34122adc72562e0323e53400adc227225
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-06-03 14:35:38 +0100

    Put in temperature reading for stm32
    From https://github.com/stm32duino/Arduino_Core_STM32/pull/539

--- src/temperature.cpp
+++ src/temperature.cpp
@@ -1,19 +1,59 @@
 #include "temperature.hpp"
+#include "stm32yyxx_ll_adc.h"
+
+/* Values available in datasheet */
+#define CALX_TEMP 25
+#if defined(STM32F1xx)
+#define V25 1430
+#define AVG_SLOPE 4300
+#define VREFINT 1200
+#endif
+
+/* Analog read resolution */
+#if ADC_RESOLUTION == 10
+#define LL_ADC_RESOLUTION LL_ADC_RESOLUTION_10B
+#define ADC_RANGE 1024
+#else
+#define LL_ADC_RESOLUTION LL_ADC_RESOLUTION_12B
+#define ADC_RANGE 4096
+#endif
+
+static int32_t readVref()
+{
+#ifdef __LL_ADC_CALC_VREFANALOG_VOLTAGE
+  return (__LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION));
+#else
+  return (VREFINT * ADC_RANGE / analogRead(AVREF)); // ADC sample to mV
+#endif
+}
+
+#ifdef ATEMP
+static int32_t readTempSensor(int32_t VRef)
+{
+#ifdef __LL_ADC_CALC_TEMPERATURE
+  return (__LL_ADC_CALC_TEMPERATURE(VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
+#elif defined(__LL_ADC_CALC_TEMPERATURE_TYP_PARAMS)
+  return (__LL_ADC_CALC_TEMPERATURE_TYP_PARAMS(AVG_SLOPE, V25, CALX_TEMP, VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
+#else
+  return 0;
+#endif
+}
+#endif
+
+static int32_t readVoltage(int32_t VRef, uint32_t pin)
+{
+  return (__LL_ADC_CALC_DATA_TO_VOLTAGE(VRef, analogRead(pin), LL_ADC_RESOLUTION));
+}
 
 int get_temperature()
 {
-    int wADC;
-    int temp;
+  int temp;
+  analogReadResolution(ADC_RESOLUTION);
+  int32_t VRef = readVref();
 
-    ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
-    ADCSRA |= _BV(ADEN);
-    delay(20);
-    ADCSRA |= _BV(ADSC);
-    while (bit_is_set(ADCSRA, ADSC))
-    {
-    }
-    wADC = ADCW;
-    temp = (wADC - 322.2) / 1.43;
+#ifdef ATEMP
+  temp = readTempSensor(VRef);
+#endif
 
-    return temp;
+  return temp;
 }
