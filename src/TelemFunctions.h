6616471f06a26959064cebb6d38db59e6bb3a7b3
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-06-03 20:53:33 +0100

    Revert "Clean up: use clamp function"
    This reverts commit 52e2c0e9c69762d1242679304e9f23e9b62789b9.

--- src/TelemFunctions.h
+++ src/TelemFunctions.h
@@ -115,11 +115,6 @@ void call_telem()
   call_telemetry[5] = l + 'A';
 }
 
-float clamp(float n, float lower, float upper)
-{
-  return max(lower, min(n, upper));
-}
-
 void loc_dbm_telem()
 {
   Sats = gps.satellites.value();
@@ -131,8 +126,23 @@ void loc_dbm_telem()
   delay(20);
   float volt = get_solar_voltage();
 
-  volt = clamp(volt, 3.0, 4.94);
-  temp = clamp(temp, -49, 39);
+  if (volt < 3.0)
+  {
+    volt = 3.0;
+  }
+  if (volt > 4.95)
+  {
+    volt = 4.95;
+  }
+
+  if (temp < -49)
+  {
+    temp = -49;
+  }
+  if (temp > 39)
+  {
+    temp = 39;
+  }
 
   int GPS = 0;
   if ((lon != oldlon) || (lat != oldlat))
