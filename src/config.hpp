50850701ce334d20addf5517bf5b2c70a23cac51
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-06-25 17:39:42 +0100

    Create config.hpp

---
+++ src/config.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file config.hpp
+ * @author Medad Newman (medad@medadnewman.co.uk)
+ * @brief
+ * @version 0.1
+ * @date 2022-06-25
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#define CALL_SIGN "SA7BSS"
+#define SI5351_TCXO_FREQUENCY 25000000
